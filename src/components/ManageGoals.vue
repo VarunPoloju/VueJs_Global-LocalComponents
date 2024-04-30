8f1a12c64750e830fe572b1eddc6666aefac9850
varun <varunpoloju966@gmail.com>
2024-04-30 19:25:12 +0530

    fragments - from vue3 onwards there is no need to keep div or any thing to wrap up the top level elements

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
 
     <!-- teleport makes where th html added to which part of html in dom -->
-    <teleport to="body">
+    <teleport to="">
       <error-alert v-if="inputIsInvalid">
         <h2>Input is Invalid</h2>
         <p>Please enter few chars...</p>
         <button @click="confirmError">Okay</button>
       </error-alert>
     </teleport>
-  </div>
 </template>
 
 <script>
