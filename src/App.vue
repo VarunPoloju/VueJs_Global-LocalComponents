e3d8a7fc3c8ec845b5c154d79f139e367ddb7cf4
varun <varunpoloju966@gmail.com>
2024-04-30 18:43:00 +0530

    dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,40 @@
   <div>
     <the-header></the-header>
     <!-- <TheHeader/> -->
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
 
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps["anotherProp"] }}</p>
-    </course-goals>
+    <!-- dynamic component below -->
+    <component :is="selectedComponent"> </component>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
-import BadgeList from "./components/BadgeList.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     TheHeader,
-    UserInfo,
-    BadgeList,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+
+  methods: {
+    setSelectedComponent(comp) {
+      this.selectedComponent = comp;
+    },
   },
 };
 </script>
