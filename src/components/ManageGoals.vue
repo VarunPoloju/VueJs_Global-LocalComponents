d7023bce78626248d9e2e4de90cc1cb8b940a358
varun <varunpoloju966@gmail.com>
2024-04-30 19:22:34 +0530

    Teleporting elements direct to body tag of html

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,11 +3,15 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="inputIsInvalid">
-      <h2>Input is Invalid</h2>
-      <p>Please enter few chars...</p>
-      <button @click="confirmError">Okay</button>
-    </error-alert>
+
+    <!-- teleport makes where th html added to which part of html in dom -->
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is Invalid</h2>
+        <p>Please enter few chars...</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
