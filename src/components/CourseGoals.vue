73b212284cf79f2c272ec8df0f56589c6dda1a81
varun <varunpoloju966@gmail.com>
2024-04-30 18:21:47 +0530

    scoped slots feature

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" anotherProp="Hello!!"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["Learning Vue", "Learning man!"],
+    };
+  },
+};
+</script>
